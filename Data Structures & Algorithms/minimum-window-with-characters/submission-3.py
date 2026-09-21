fde5bf175a9acc799d62dba5b728b60151902303
atuln98 <76964035+atuln98@users.noreply.github.com>
2026-09-21 20:14:54 +0530

    Add: minimum-window-with-characters - submission-3

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-3.py
@@ -0,0 +1,37 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        cs={}
+        ct={}
+        out=0
+        besti=0
+        bestj=0
+        have = 0
+        for i in t:
+            ct[i]=ct.get(i,0)+1
+        i=j=0
+        need = len(ct)
+        while j<len(s):
+            if s[j] in ct:
+                cs[s[j]]=cs.get(s[j],0)+1
+                if cs[s[j]]==ct[s[j]]:
+                    have+=1
+            while have == need:
+                if out==0:
+                    out = j-i+1
+                    besti=i
+                    bestj=j
+                elif out>j-i+1:
+                    out = j-i+1
+                    besti=i
+                    bestj=j
+                if s[i] in ct:
+                    cs[s[i]]-=1
+                    if cs[s[i]]<ct[s[i]]:
+                        have-=1
+                i+=1
+
+            j+=1
+        if out==0:
+            return ""
+        return s[besti:bestj+1]
+        
